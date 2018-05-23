440a50e7fb49c3b7a0cd66f0b3db7b38ec50047a
Jan van Deventer <jan.van.deventer@ltu.se>
2018-05-23 10:38:36 +0200

    Introduce TimerCounter 2 and internal interrupts.

--- README.md
+++ README.md
@@ -14,6 +14,16 @@ It is only at the end of the course that we introduce micro controllers and syst
 The text below this line in this README file is updated with each commit to enable a student to navigate rapidly between the diffent branch and commits.
 
 # Version's aim
-This commit is where we start to branch out.
+This version of the software it to introduce internal interrupts.
+To achieve this, we use Timer and Counters (which we thereby introduce).
+In this present case, LED3 is flashed on and off.
 
 ## Exercises
+
+- There are three crytal oscillators on the course development board. Using an oscilloscope in the lab, try to mearure their "beat".
+- Measure the square pulse wave between LED3 and R20 on the board. What are the proportions of ON and OFF times? What is the time of each state? What is the frequency?
+- Change the pre-scalor and check on the oscilloscope the change in pulse.
+- Change the Output Compare Regiter and see how that affects the square wave.
+- Change the mode of the TimerCounter and see how that affects the square wave.
+
+- You should try to add the code with delay and see that a delay does not affect the execution of the Interrupt Sercice Routines (ISR).
--- main.c
+++ main.c
@@ -2,50 +2,71 @@
  * AS2p.c
  *
  * Created: 5/15/2018 10:00:41 AM
- * Modified: May 17, 2018
+ * Modified: May 22, 2018
  * Author : Jan van Deventer
  * Course: E0009E Automotive Systems 2
  */ 
 
-#include <avr/io.h> // input output header file for this AVR chip.
-#include "gpio.h"
-
 /*
  * Purpose of this version:
- * The purpose of this version of the software package is to introduce the idea of the include files.
- * The code is the same as that of the previous version but spread in different files.
+ * The purpose of this version of the software package is to introduce internal interrupts.
+ * It does that by using Timer Counters. (You might should use an oscilloscope connected between LED3 and R20.)
 */
 
+volatile unsigned char buttons;		// This registers holds a copy of PINC when an external interrupt 6 has occurred.
+volatile unsigned char bToggle = 0;	// This registers is a boolean that is set when an interrupt 6 occurs and cleared when serviced in the code.
+//These "volatile" registers is available outside of the main loop (i.e., to the interrupt handlers)
 
+#include <avr/io.h> // input output header file for this AVR chip.
+#include <avr/pgmspace.h>	// Contains some type definitions and functions.
+#include <avr/interrupt.h>	// Contains ISR (Interrupt Service Routines) or interrupt handler details.
+#include "gpio.h"	// set the data direction of our io ports.
+
+int initExtInt(void)
+{
+	//Set up external Interrupts
+	// The five Switches are ORed to Pin PE6 which is alternatively Int6
+	EICRB |= (0<<ISC61) | (1<<ISC60);  //Any logical change to INT6 generates an interrupt
+	EIMSK |= (1<<INTF6);
+	return(6);
+}
+
+
+int initTimer2()
+{
+	/// Set up an internal Interrupt that will occur every 5 milliseconds.
+	/// It uses the Timer Counter 2 in CTC mode with a pre-scaler of 256 and a value of 155 (it should be 155.25).
+	//
+	TCCR2A = (1<<WGM21); // | (0<<WGM20);  //CTC mode
+	//TCCR2A |= (0<<COM2A1) | (0<<COM2A0); // Mormal port operation, OC2A is disconnected.
+	TCCR2A |= (1<<CS22) | (1<<CS21); //| (0<<CS20); /// Divide source frequency source by 256.
+	TCNT2 = 0;	/// Make sure the timer counter is set to 0.
+	OCR2A = 100;
+	TIMSK2 = (1<<OCF2A); // Interrupt flag register to enable output compare.
+	return(2);
+}
 
 
 int main(void)
 {
-	unsigned char temp ;		//Allocate memory for  temp
-	
-	temp = initGPIO();				//Set up the data direction register for both ports C and G
-	
+	unsigned char temp = 0x0F;		// Allocate memory for temp. It is initialized to 15 for demonstration purposes only.
+		
+	temp = initGPIO();				// Set up the data direction register for both ports C and G
+	temp = initTimer2();			// Setup 5ms internal interrupt
+	sei();							// Set Global Interrupts
+		
 	while(1)
 	{
-			temp = PINC;			// Copy Input Port C register (PINC) to temp.
-			temp &= 0b11111000;		// Prepare to turn off Port C LEDs
-			
-			if (temp & 0b10000000)
-				temp |= 0b00000100;		// Prepare to turn on Led5 if S5 is on
-			if (temp & 0b01000000)
-				temp |= 0b00000010;		// Prepare to turn on Led4 if S4 is on
-			if (temp & 0b00100000)
-				temp |= 0b00000001;		// Prepare to turn on Led3 if S3 is on			
-			PORTC = temp & 0b00000111;	// Copy the last 3 bits of temp to Port C to turn on the LEDs.
-			
-			temp &= 0b11111000;		//Clear all LEDs so we do not turn on what we do not want
-			if (temp & 0b00010000)
-				temp |= 0b00000010;		// Prepare to turn on Led2 if S1 is on
-			if (temp & 0b00001000)
-				temp |= 0b00000001;		// Prepare to turn on Led1 if S1 is on			
-			temp &= 0b00000011;		// Clear the upper bits of temp to then turn on only the 2 LEDs in the next line
-			PORTG &= 0b11111100;	// Turn off the LEDs if they are on.
-			PORTG |= temp;			// Copy the last 2 bits of temp to Port G to turn on the LEDs.
+		temp++;
 	}
+
 	return(0); // This line of code will never be executed since it is after the "while(1)" block
+}
+
+SIGNAL(SIG_OUTPUT_COMPARE2)
+{
+	if (PINC & 0x01)
+		PORTC &= 0x00;	//Turn on Led3 if S3 is on
+	else
+		PORTC |= 0x01;
 }
