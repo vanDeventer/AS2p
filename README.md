5a4342fc6d6df870778a3909971908471701db71
Jan van Deventer <jan.van.deventer@ltu.se>
2018-05-28 14:32:20 +0200

    Introduction of the ADC peripheral.

--- README.md
+++ README.md
@@ -14,6 +14,16 @@ It is only at the end of the course that we introduce micro controllers and syst
 The text below this line in this README file is updated with each commit to enable a student to navigate rapidly between the diffent branch and commits.
 
 # Version's aim
-This commit is where we start to branch out.
+In this commit, we introduce the Analog to Digital Converter.
+We use the potentiometers on the development board to change the potential of the analog signal with respect to ground.
+The user must add a wire or jumger between POT1 or POT2 to the ADC pin header.
+To know which pin to use, one must refer tot he schematic of the board.
+Initially, the code uses ADC0.
+The value is displayed using the 5 LEDs.
 
 ## Exercises
+- Reverse the order of the LEDs as a function of the analog value.
+- Change the input from ADC0 to ADC1 or ADC2.
+- Using both potentiometers, take the difference ADC1 - ADC2.
+- Alternate between ADC0 and ADC1 at runtime.
+- Move to ADC setup routine to a .h and .c pair of files and run the that code. (Do not forget to add the files so your solution.)
--- main.c
+++ main.c
@@ -2,50 +2,90 @@
  * AS2p.c
  *
  * Created: 5/15/2018 10:00:41 AM
- * Modified: May 17, 2018
+ * Modified: May 28, 2018
  * Author : Jan van Deventer
  * Course: E0009E Automotive Systems 2
  */ 
 
-#include <avr/io.h> // input output header file for this AVR chip.
-#include "gpio.h"
 
 /*
  * Purpose of this version:
- * The purpose of this version of the software package is to introduce the idea of the include files.
- * The code is the same as that of the previous version but spread in different files.
+ * The purpose of this version of the software package is to introduce the Analog to Digital Converter (ADC)
+ * The value of the analog signal is displayed using the five LEDs.
+ * You will need a wire or jumper connecting POT1 or POT2 and ADC0.
 */
 
+#include <avr/io.h> // input output header file for this AVR chip.
+#include <avr/pgmspace.h>	// Contains some type definitions and functions.
+#include <avr/interrupt.h>	// Contains ISR (Interrupt Service Routines) or interrupt handler details
+#include "gpio.h"
+
+volatile uint16_t adc_value;  //Allocate the double byte memory space into which the result of the 10 bits Analog to Digital Converter (ADC) is stored.
+
+int initADC(){
+	//Set up analog to digital conversion (ADC)
+	//ADMUX register
+	//AVcc with external capacitor on AREF pin (the 2 following lines)
+	ADMUX &= ~(1<<REFS1);  //Clear REFS1 (although it should be 0 at reset)
+	ADMUX |= (1<<REFS0);   //Set REFS0
+	ADMUX &= (0b11100000); //Single ended input on ADC0
+	ADMUX &= ~(1<<ADLAR);  //Making sure ADLAR is zero (somehow it was set to 1)
+	//The ACDC control and status register B ADCSRB
+	ADCSRB &= ~(1<<ADTS2) & ~(1<<ADTS1) & ~(1<<ADTS0);  //Free running mode
+	//The ADC control and status register A ADCSRA
+	ADCSRA |= (1<<ADPS2) | (1<<ADPS1) |(1<<ADPS0);//set sampling frequency pre-scaler to a division by 128
+	ADCSRA |= (1<<ADEN)  | (1<<ADATE) | (1<<ADIE);//enable ADC, able ADC auto trigger, enable ADC interrupt
+	return(0);
+}
 
 
 
 int main(void)
 {
-	unsigned char temp ;		//Allocate memory for  temp
+	unsigned char temp;			//Allocate memory for  temp
 	
-	temp = initGPIO();				//Set up the data direction register for both ports C and G
+	temp = initGPIO();			//Set up the data direction register for both ports B, C and G
+	temp = initADC();			// Setup the Analog to Digital Converter
+	ADCSRA |= (1<<ADSC);		//Start ADC
+
+	sei();						// Set Global Interrupts
+	
 	
 	while(1)
 	{
-			temp = PINC;			// Copy Input Port C register (PINC) to temp.
-			temp &= 0b11111000;		// Prepare to turn off Port C LEDs
-			
-			if (temp & 0b10000000)
-				temp |= 0b00000100;		// Prepare to turn on Led5 if S5 is on
-			if (temp & 0b01000000)
-				temp |= 0b00000010;		// Prepare to turn on Led4 if S4 is on
-			if (temp & 0b00100000)
-				temp |= 0b00000001;		// Prepare to turn on Led3 if S3 is on			
-			PORTC = temp & 0b00000111;	// Copy the last 3 bits of temp to Port C to turn on the LEDs.
-			
-			temp &= 0b11111000;		//Clear all LEDs so we do not turn on what we do not want
-			if (temp & 0b00010000)
-				temp |= 0b00000010;		// Prepare to turn on Led2 if S1 is on
-			if (temp & 0b00001000)
-				temp |= 0b00000001;		// Prepare to turn on Led1 if S1 is on			
-			temp &= 0b00000011;		// Clear the upper bits of temp to then turn on only the 2 LEDs in the next line
-			PORTG &= 0b11111100;	// Turn off the LEDs if they are on.
-			PORTG |= temp;			// Copy the last 2 bits of temp to Port G to turn on the LEDs.
+		cli();					// Disable the global interrupts to prevent accidental corruption of the results while the two bytes.
+		if (adc_value>852){
+			PORTC = 0b00000111;
+			PORTG |= 0b00000011;
+		}
+		else if(adc_value>682){
+			PORTC = 0b00000011;
+			PORTG |= 0b00000011;
+		}
+		else if(adc_value>511){
+			PORTC = 0b00000001;
+			PORTG |= 0b00000011;			
+		}
+		else if(adc_value>341){
+			PORTC = 0b00000000;
+			PORTG |= 0b00000011;
+		}
+		else if(adc_value>170){
+			PORTC = 0b00000000;
+			PORTG |= 0b00000001;
+			PORTG &= 0b11111101;
+		}
+		else{
+			PORTC = 0b00000000;
+			PORTG &= 0b00000000;
+		};
+		
+		sei();					// re-enable the global interrupts
 	}
 	return(0); // This line of code will never be executed since it is after the "while(1)" block
+}
+
+ISR(ADC_vect){
+	adc_value = ADCL;		//Load the low byte of the ADC result
+	adc_value += (ADCH<<8); //shift the high byte by 8bits to put the high byte in the variable
 }
